5fed7d6a48d17879038b9191d3ad2a005bbdcea7
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2022-11-11 00:21:20 +0200

    Completes task 1 HW-09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,12 +2,23 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-function setBackground(){
+let interval;
 
+function setBackgroundColor(){
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    interval = setInterval(()=>{
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
 }
 
-startBtn = document.querySelector("button[data-start]");
-debugger
-console.log(startBtn);
-stopBtn = document.querySelector("button[data-stop]");
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+stopBtn.disabled = true;
 
+startBtn.addEventListener("click", setBackgroundColor);
+stopBtn.addEventListener("click", ()=>{ 
+    clearInterval(interval); 
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+});
